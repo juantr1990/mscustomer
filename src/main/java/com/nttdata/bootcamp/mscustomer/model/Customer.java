4e99d45095a3bd644c14a8dc38c20e19a0249d69
Juan Torres Romero <jtorrrom@emeal.nttdata.com>
2022-10-12 17:20:32 -0500

    Customer ms base

---
+++ src/main/java/com/nttdata/bootcamp/mscustomer/model/Customer.java
@@ -0,0 +1,21 @@
+package com.nttdata.bootcamp.mscustomer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    private Integer code;
+    private String name;
+    private String lastname;
+}
