910632b748f2586d5b572072b1e0b82ff7b77bd5
Juan Torres Romero <jtorrrom@emeal.nttdata.com>
2022-10-14 09:56:41 -0500

    RETRIEVE ALL CUSTOMER

--- src/main/java/com/nttdata/bootcamp/mscustomer/application/CustomerController.java
+++ src/main/java/com/nttdata/bootcamp/mscustomer/application/CustomerController.java
@@ -3,7 +3,9 @@ package com.nttdata.bootcamp.mscustomer.application;
 import com.nttdata.bootcamp.mscustomer.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -16,5 +18,10 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Customer> insertCustomer(@RequestBody Customer customer){
         return customerService.insertCustomer(Mono.just(customer));
+    }
+
+    @GetMapping(value = "employee", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Customer> retrieveAll(){
+        return customerService.retrieveAll();
     }
 }
