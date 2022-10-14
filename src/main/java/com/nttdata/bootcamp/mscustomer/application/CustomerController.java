744f6e6d4aa7a7121e42bda76e8e534ad2609f30
Juan Torres Romero <jtorrrom@emeal.nttdata.com>
2022-10-14 17:42:59 -0500

    config server

--- src/main/java/com/nttdata/bootcamp/mscustomer/application/CustomerController.java
+++ src/main/java/com/nttdata/bootcamp/mscustomer/application/CustomerController.java
@@ -2,6 +2,8 @@ package com.nttdata.bootcamp.mscustomer.application;
 
 import com.nttdata.bootcamp.mscustomer.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +11,14 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RefreshScope
 public class CustomerController {
 
     @Autowired
     CustomerService customerService;
+    
+    @Value("${message.demo}") 
+    private String demoString;
 
     @PostMapping("customer")
     @ResponseStatus(HttpStatus.CREATED)
@@ -20,7 +26,7 @@ public class CustomerController {
         return customerService.insertCustomer(Mono.just(customer));
     }
 
-    @GetMapping(value = "employee", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "customer", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Customer> retrieveAll(){
         return customerService.retrieveAll();
     }
