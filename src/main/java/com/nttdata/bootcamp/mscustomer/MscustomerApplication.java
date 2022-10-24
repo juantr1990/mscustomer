1996c0b13d48ab1c3a62c727df5d86c8d5215666
Juan Torres Romero <jtorrrom@emeal.nttdata.com>
2022-10-24 17:20:02 -0500

    Eureka Registering

--- src/main/java/com/nttdata/bootcamp/mscustomer/MscustomerApplication.java
+++ src/main/java/com/nttdata/bootcamp/mscustomer/MscustomerApplication.java
@@ -2,8 +2,12 @@ package com.nttdata.bootcamp.mscustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MscustomerApplication {
 
 	public static void main(String[] args) {
